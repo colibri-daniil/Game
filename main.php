8916a985b6c266887f93392dc21fc1b8c9c7f1de
colibri-daniil <59998669+colibri-daniil@users.noreply.github.com>
2020-02-15 12:28:26 +0200

    New update

--- main.php
+++ main.php
@@ -12,12 +12,15 @@ $col_1 = $columns->addColumn(2);
 $col_2 = $columns->addColumn(12);
 $col_3 = $columns->addColumn(2);
 
+$val = $col_2->add(["FormField/Line", "45"]);
+
 $clicker = $col_2->add(["Button",$_SESSION["i"],"blue fluid big"]);
-$clicker->on("click", function ($n) {
-$_SESSION["i"] = $_SESSION["i"] + 1;
-return $n;
-});
 $save = $col_2->add(["Button","Save","green big"]);
+$clicker->js("click", new \atk4\ui\jsReload($val, ["val" => $val->jsInput()->val(new \atk4\ui\jsExpression("parseInt([])+1", [$val->jsInput()->val()]), $val->jsInput()->focus())]));
 
 $exit = $app->add(["Button","Exit","red"]);
 $exit->link(["exit"]);
+
+$x2 = $col_3->add(["Button","click x2","inverted mediun yellow"]);
+
+$pus = $col_3->add(["Button","+0.5 cli/sek","inverted mediun purple"]);
